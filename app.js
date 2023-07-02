d1e8b7853a05bb05d2cef92dac835f25ebb7550f
Hoowerr <109082395+Hoowerr@users.noreply.github.com>
2023-07-02 14:56:55 +0300

    fix cors

--- app.js
+++ app.js
@@ -8,19 +8,28 @@ const errorHandler = require("./middlewares/errorHandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const limiter = require("./utils/limiter");
 const config = require("./utils/config");
-
-const allowedCors = [
-  "http://localhost:3000",
-  "https://movies-explorer.hoower.nomoredomains.rocks",
-  "http://158.160.3.169:3000",
-  "localhost",
-  "localhost:3000",
-  "http://localhost",
-];
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://movies-explorer.hoower.nomoredomains.rocks",
+      "http://158.160.3.169:3000",
+      "localhost",
+      "localhost:3000",
+      "http://localhost",
+    ],
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    optionsSuccessStatus: 204,
+    preflightContinue: false,
+  })
+);
 
 mongoose
   .connect(config.MONGODB_PATH, {
@@ -33,23 +42,9 @@ mongoose
     console.log("Не удалось подключиться к БД");
   });
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers["access-control-request-headers"];
-  res.header("Access-Control-Allow-Credentials", true);
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
-    res.end();
-  }
-  next();
-});
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
